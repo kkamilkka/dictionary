693cb33c00b5be1470fc9e164fcd4a63f084fdd4
Kamila <kamila.kowacz@gmail.com>
2021-10-01 14:01:40 +0200

    update select fuction

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
+from sqlalchemy import func, select
+from sqlalchemy.sql import and_
 import random
+from sqlalchemy.dialects import postgresql
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kamila@localhost:5432/dictionary'
@@ -27,7 +29,13 @@ def index():
     rand = db.session.query(func.count(Todo.id)).scalar()
     random_number = random.randrange(0, rand)
 
-    return render_template('index.html', rand=rand, random_number=random_number)
+    stmt = select([Todo.__table__.columns.description1, Todo.__table__.columns.description2])
+    stmt = stmt.where(and_(Todo.id == random_number))
+    result = stmt.compile(dialect=postgresql.dialect())
+    print (result)
+    # print(stmt)
+
+    return render_template('index.html', rand=rand, random_number=random_number, result=result)
 
 
 if __name__ == '__main__':
