4b537b617e369a447a9dd4bfa5fc8f98ca56fa07
Kamila <kamila.kowacz@gmail.com>
2021-11-09 11:11:31 +0100

    create form

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect, abort
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func, select
-from sqlalchemy.sql import and_
+from sqlalchemy import func
 import random
-from sqlalchemy.dialects import postgresql
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kamila@localhost:5432/dictionary'
@@ -18,24 +16,34 @@ class Todo(db.Model):
     description2 = db.Column(db.String(), nullable=False)
 
     def __repr__(self):
-        return f'< id: {self.id}, description: {self.description1}, description: {self.description2}>'
+        global nl_trans
+        nl_trans = self.description2
+        return f'{self.description1}'
 
 
 db.create_all()
 
 
+@app.route('/form', methods=['post'])
+def form():
+    pl_translation = request.form['pl_translation']
+    nl_translation = request.form['nl_translation']
+    if nl_translation == nl_trans:
+        return render_template('goed.html', pl_translation=pl_translation, nl_translation=nl_translation)
+    else:
+        print('zle')
+
+    #return render_template('index.html')
+
+
 @app.route('/')
 def index():
     rand = db.session.query(func.count(Todo.id)).scalar()
-    random_number = random.randrange(0, rand)
+    random_number = random.randrange(1, rand + 1)
 
-    stmt = select([Todo.__table__.columns.description1, Todo.__table__.columns.description2])
-    stmt = stmt.where(and_(Todo.id == random_number))
-    result = stmt.compile(dialect=postgresql.dialect())
-    print (result)
-    # print(stmt)
+    output = Todo.query.get(random_number)
 
-    return render_template('index.html', rand=rand, random_number=random_number, result=result)
+    return render_template('index.html', output=output)
 
 
 if __name__ == '__main__':
